ec37ac0d2359630d560392cfcf898eb70ad409ef
KatterMaw <narcat99@gmail.com>
2024-02-09 01:33:11 +0500

    Encapsulate ArchetypeBuilder

---
+++ ECS/ArchetypeBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace ECS;
+
+internal sealed class ArchetypeBuilder
+{
+	public ArchetypeBuilder Add<TComponent>() where TComponent : struct
+	{
+		var componentType = ComponentType.Get<TComponent>();
+		_componentTypes.Add(componentType);
+		_listsBuilder.Add(componentType, new List<TComponent>());
+		return this;
+	}
+
+	public Archetype Build(World world)
+	{
+		ArchetypeDescription archetypeDescription = new(_componentTypes);
+		Archetype archetype = new(_listsBuilder.ToImmutable());
+		world.Archetypes.Add(archetypeDescription, archetype);
+		return archetype;
+	}
+
+	private readonly List<ComponentType> _componentTypes = new();
+	private readonly ImmutableDictionary<ComponentType, IList>.Builder _listsBuilder =
+		ImmutableDictionary.CreateBuilder<ComponentType, IList>();
+}
--- ECS/EntityBuilder.cs
+++ ECS/EntityBuilder.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections;
-using System.Collections.Immutable;
+using CommunityToolkit.Diagnostics;
 
 namespace ECS;
 
@@ -10,21 +10,24 @@ public sealed class EntityBuilder
 		public static ComponentFactory Create<TComponent>(TComponent component) where TComponent : struct
 		{
 			var componentType = ComponentType.Get<TComponent>();
-			return new ComponentFactory(componentType, CreateList, AddComponent);
-			List<TComponent> CreateList() => new();
+			return new ComponentFactory(componentType, AddComponent);
 			void AddComponent(IList list) => ((IList<TComponent>)list).Add(component);
 		}
 		
 		public ComponentType Type { get; }
-		public Func<IList> CreateList { get; }
 		public Action<IList> AddComponent { get; }
 
-		private ComponentFactory(ComponentType type, Func<IList> createList, Action<IList> addComponent)
+		private ComponentFactory(ComponentType type, Action<IList> addComponent)
 		{
 			Type = type;
-			CreateList = createList;
 			AddComponent = addComponent;
 		}
+	}
+
+	public EntityBuilder(bool buildArchetype = true)
+	{
+		if (buildArchetype)
+			_archetypeBuilder = new ArchetypeBuilder();
 	}
 	
 	public EntityBuilder Add<TComponent>(TComponent component = default) where TComponent : struct
@@ -41,21 +44,13 @@ public sealed class EntityBuilder
 	}
 
 	private readonly Dictionary<ComponentType, ComponentFactory> _componentFactories = new();
+	private readonly ArchetypeBuilder? _archetypeBuilder;
 
 	private Archetype GetOrCreateArchetype(World world, ArchetypeDescription archetypeDescription)
 	{
 		if (world.Archetypes.TryGetValue(archetypeDescription, out var archetype))
 			return archetype;
-		return BuildArchetype(world, archetypeDescription);
-	}
-
-	private Archetype BuildArchetype(World world, ArchetypeDescription archetypeDescription)
-	{
-		var componentLists = _componentFactories.ToImmutableDictionary(
-			pair => pair.Key,
-			pair => pair.Value.CreateList());
-		Archetype archetype = new(componentLists);
-		world.Archetypes.Add(archetypeDescription, archetype);
-		return archetype;
+		Guard.IsNotNull(_archetypeBuilder);
+		return _archetypeBuilder.Build(world);
 	}
 }
