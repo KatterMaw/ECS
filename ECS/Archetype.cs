2bd90add849662293ec43d7cbe85887f802fdf70
KatterMaw <narcat99@gmail.com>
2024-02-19 07:10:44 +0500

    Encapsulate components related stuff from Archetype to ComponentsList

--- ECS/Archetype.cs
+++ ECS/Archetype.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
-using CommunityToolkit.Diagnostics;
-using ECS.Extensions;
+﻿using System.Runtime.InteropServices;
 
 namespace ECS;
 
@@ -16,30 +11,19 @@ public sealed class Archetype
 		return CollectionsMarshal.AsSpan(_entities);
 	}
 	
-	public bool Has<TComponent>() where TComponent : struct
-	{
-		return _components[Component<TComponent>.Type.Id] != null;
-	}
-
 	public Span<TComponent> GetSpan<TComponent>() where TComponent : struct
 	{
-		return CollectionsMarshal.AsSpan(GetList<TComponent>());
-	}
-
-	public bool TryGetSpan<TComponent>(out Span<TComponent> span) where TComponent : struct
-	{
-		span = Span<TComponent>.Empty;
-		if (!TryGetList<TComponent>(out var list))
-			return false;
-		span = CollectionsMarshal.AsSpan(list);
-		return true;
+		return _components.GetSpan<TComponent>();
 	}
 
 	public Span<TComponent> GetOptionalSpan<TComponent>() where TComponent : struct
 	{
-		if (TryGetSpan<TComponent>(out var span))
-			return span;
-		return Span<TComponent>.Empty;
+		return _components.GetOptionalSpan<TComponent>();
+	}
+
+	public bool Has<TComponent>() where TComponent : struct
+	{
+		return _components.Has<TComponent>();
 	}
 
 	internal ArchetypeDescription Description { get; }
@@ -47,68 +31,28 @@ public sealed class Archetype
 	internal Archetype(ArchetypeDescription archetypeDescription)
 	{
 		Description = archetypeDescription;
-		var componentTypes = archetypeDescription.ComponentTypes;
-		var requiredSize = componentTypes.GetMaxId() + 1;
-		var builder = ImmutableArray.CreateBuilder<IList?>(requiredSize);
-		builder.Count = requiredSize;
-		foreach (var componentType in componentTypes)
-			builder[componentType.Id] = componentType.List;
-		_components = builder.ToImmutable();
-		_types = componentTypes.ToImmutableArray();
+		_components = new ComponentsList(archetypeDescription.ComponentTypes);
 	}
 	
 	internal ref Entity CreateEntity(List<ComponentFactory> componentFactories)
 	{
-		AddComponents(componentFactories);
+		_components.AddComponents(componentFactories);
 		return ref CreateEntity();
 	}
 
 	internal ref TComponent Get<TComponent>(int index) where TComponent : struct
 	{
-		return ref GetSpan<TComponent>()[index];
+		return ref _components.Get<TComponent>(index);
 	}
 
 	internal void EnsureRemainingCapacity(int capacity)
 	{
 		_entities.EnsureCapacity(_entities.Count + capacity);
-		foreach (var type in _types)
-		{
-			var list = _components[type.Id];
-			Guard.IsNotNull(list);
-			type.EnsureListRemainingCapacity(list, capacity);
-		}
+		_components.EnsureRemainingCapacity(capacity);
 	}
 
 	private readonly List<Entity> _entities = new();
-	private readonly ImmutableArray<IList?> _components;
-	private readonly ImmutableArray<ComponentType> _types;
-
-	private List<TComponent> GetList<TComponent>() where TComponent : struct
-	{
-		var list = _components[Component<TComponent>.Type.Id];
-		Guard.IsNotNull(list);
-		return (List<TComponent>)list;
-	}
-
-	private bool TryGetList<TComponent>([NotNullWhen(true)] out List<TComponent>? list) where TComponent : struct
-	{
-		list = null;
-		var index = Component<TComponent>.Type.Id;
-		if (_components.Length <= index)
-			return false;
-		list = _components[index] as List<TComponent>;
-		return list != null;
-	}
-
-	private void AddComponents(List<ComponentFactory> componentFactories)
-	{
-		foreach (var componentFactory in componentFactories)
-		{
-			var list = _components[componentFactory.ComponentType.Id];
-			Guard.IsNotNull(list);
-			componentFactory.AddComponent(list);
-		}
-	}
+	private readonly ComponentsList _components;
 
 	private ref Entity CreateEntity()
 	{
@@ -120,6 +64,6 @@ public sealed class Archetype
 
 	private ref Entity GetEntity(int index)
 	{
-		return ref GetEntitiesSpan()[index];
+		return ref GetEntities()[index];
 	}
 }
---
+++ ECS/ComponentsList.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using CommunityToolkit.Diagnostics;
+using ECS.Extensions;
+
+namespace ECS;
+
+internal sealed class ComponentsList
+{
+	internal ComponentsList(IReadOnlyCollection<ComponentType> componentTypes)
+	{
+		_componentTypes = componentTypes.ToImmutableArray();
+		var requiredSize = componentTypes.GetMaxId() + 1;
+		var builder = ImmutableArray.CreateBuilder<IList?>(requiredSize);
+		builder.Count = requiredSize;
+		foreach (var componentType in componentTypes)
+			builder[componentType.Id] = componentType.List;
+		_componentLists = builder.ToImmutable();
+	}
+	
+	public bool Has<TComponent>() where TComponent : struct
+	{
+		return _componentLists[Component<TComponent>.Type.Id] != null;
+	}
+
+	public Span<TComponent> GetSpan<TComponent>() where TComponent : struct
+	{
+		return CollectionsMarshal.AsSpan(GetList<TComponent>());
+	}
+
+	public bool TryGetSpan<TComponent>(out Span<TComponent> span) where TComponent : struct
+	{
+		span = Span<TComponent>.Empty;
+		if (!TryGetList<TComponent>(out var list))
+			return false;
+		span = CollectionsMarshal.AsSpan(list);
+		return true;
+	}
+
+	public Span<TComponent> GetOptionalSpan<TComponent>() where TComponent : struct
+	{
+		if (TryGetSpan<TComponent>(out var span))
+			return span;
+		return Span<TComponent>.Empty;
+	}
+
+	internal void AddComponents(List<ComponentFactory> componentFactories)
+	{
+		foreach (var componentFactory in componentFactories)
+		{
+			var list = _componentLists[componentFactory.ComponentType.Id];
+			Guard.IsNotNull(list);
+			componentFactory.AddComponent(list);
+		}
+	}
+
+	internal ref TComponent Get<TComponent>(int index) where TComponent : struct
+	{
+		return ref GetSpan<TComponent>()[index];
+	}
+
+	internal void EnsureRemainingCapacity(int capacity)
+	{
+		foreach (var type in _componentTypes)
+		{
+			var list = _componentLists[type.Id];
+			Guard.IsNotNull(list);
+			type.EnsureListRemainingCapacity(list, capacity);
+		}
+	}
+	
+	private readonly ImmutableArray<IList?> _componentLists;
+	private readonly ImmutableArray<ComponentType> _componentTypes;
+
+	private List<TComponent> GetList<TComponent>() where TComponent : struct
+	{
+		var list = _componentLists[Component<TComponent>.Type.Id];
+		Guard.IsNotNull(list);
+		return (List<TComponent>)list;
+	}
+
+	private bool TryGetList<TComponent>([NotNullWhen(true)] out List<TComponent>? list) where TComponent : struct
+	{
+		list = null;
+		var index = Component<TComponent>.Type.Id;
+		if (_componentLists.Length <= index)
+			return false;
+		list = _componentLists[index] as List<TComponent>;
+		return list != null;
+	}
+}
